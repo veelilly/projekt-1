eac1d514dfbe4caf691d3673dcb4e635c276b3ab
veelilly <164676176+veelilly@users.noreply.github.com>
2024-05-01 23:06:35 +0200

    oprava

--- projekt_1.py
+++ projekt_1.py
@@ -19,7 +19,7 @@ registrovani_uzivatele = """
 """
 
 uzivatel_heslo = {"bob": "123",
-                  "ann": "pass12i3",
+                  "ann": "pass123",
                   "mike": "password123",
                   "liz": "pass123"}
 
@@ -32,111 +32,82 @@ uppercase = 0
 lowercase = 0
 numeric = 0
 sum_numbers = 0
+delka_slov = {}
 
 # vyžádá si přihlašovací jméno a heslo 
-if (username := input("Please insert your username: ")) and \
-    (password := input("Please insert your password: ")) \
-    in registrovani_uzivatele:
+(username := input("Please insert your username: ")) 
+(password := input("Please insert your password: "))
     
-    # zkontroluje zda jméno a heslo sedí:
-    if username in uzivatel_heslo and \
-        uzivatel_heslo[username] == password:
-    
-        # pokud jméno a heslo sedí, pozdraví a pustí ho k analýze 
-        print(oddelovac)
+# zkontroluje zda jméno a heslo sedí:
+if username in uzivatel_heslo and uzivatel_heslo[username] == password:
+    # pokud jméno a heslo sedí, pozdraví a pustí ho k analýze 
+    print(oddelovac)
+    print(f"username: {username}\npassword: {password}")
+    print(oddelovac)
+    print(f"Welcome to the app, {username} \nWe have 3 texts to be analyzed.")
+    print(oddelovac)
 
-        print(f"username: {username}\npassword: {password}")
-
-        print(oddelovac)
-
-        print(f"Welcome to the app, {username} \nWe have 3 texts to be analyzed.")
-    
-        print(oddelovac)
-
-        # výběr textu k analýze
-        enumerate(texty,start=1)
-
-        vyber = None    # pomocná proměnná
-
-        while vyber is None:
-            vstup = input("Enter a number btw. 1 and 3 to select: ")
-
-            # kontrola jestli je zadané číslo číslo
-            if vstup.isdigit():
-                cislo = int(vstup) - 1
-                if 0 <= cislo < len(texty):
-                    vyber = cislo
-
-                # pokud číslo není v zadáni
-                else:
-                    print(f" {vstup} is not a valid choice, terminating program..")
-                    break
-
-            # pokud číslo není číslo        
+    # výběr textu k analýze
+    vyber = None    # pomocná proměnná
+    while vyber is None:
+        vstup = input("Enter a number btw. 1 and 3 to select: ")
+        # kontrola jestli je zadané číslo číslo
+        if vstup.isdigit():
+            cislo = int(vstup) - 1
+            if 0 <= cislo < len(texty):
+                vyber = cislo
+            # pokud číslo není v zadáni
             else:
-                print(f" {vstup} is not a number, terminating program..")
-
-        if vyber is not None:
-            vybrany_text = texty[vyber]
+                print(f" {vstup} is not a valid choice, terminating program..")
+                break
+        # pokud číslo není číslo        
+        else:
+            print(f" {vstup} is not a number, terminating program..")
         
-            # analýza slov 
+    # analýza slov a znaků
+        vybrany_text = texty[vyber]
+        slova = vybrany_text.split()
+        for slovo in slova:
+            # vyčistíme každé slovo od čárek a teček
+            clean = slovo.strip(",.")
+            words += 1
+            if clean.istitle():
+                titlecase += 1
+            if clean.islower():
+                lowercase += 1
+            if clean.isupper() and clean.isalpha():
+                uppercase += 1
+            if clean.isnumeric():
+                numeric += 1
+            if clean.isdigit():
+                sum_numbers += int(slovo)
+            delka = len(clean)
+            if delka in delka_slov:
+                delka_slov[delka] += 1
+            else:
+                delka_slov[delka] = 1
 
-            slova = vybrany_text.split()
+        # výpis výsledků
+        print(oddelovac)
+        print(f"There are {words} words in the selected text.")
+        print(f"There are {titlecase} titlecase words.")
+        print(f"There are {uppercase} uppercase words.")
+        print(f"There are {lowercase} lowercase words.")
+        print(f"There are {numeric} numeric strings.")
+        print(f"The sum of all the numbers {sum_numbers}.")
+        print(oddelovac)
+        print("LEN|\tOCCURENCES\t|NR.")
+        print(oddelovac)
 
-            # analýza znaků
-            for slovo in slova:
-                # vyčistíme každé slovo od čárek a teček
-                clean = slovo.strip(".,")
-                words += 1
-                if slovo.istitle():
-                    titlecase += 1
-                if slovo.isupper():
-                    uppercase += 1
-                if slovo.islower():
-                    lowercase += 1
-                if slovo.isnumeric():
-                    numeric += 1
-                if slovo.isdigit():
-                    sum_numbers += int(slovo)
+        # sloupcový graf
+        for i, pocet in sorted(delka_slov.items()):
+            if pocet > 0:
+                stars = "*" * pocet
+                print(f"{i+1:<3}| {stars:<13}\t|{pocet}")   
 
-            print(oddelovac)
-
-            # výpis výsledků
-            print(f"There are {words} words in the selected text.")
-            print(f"There are {titlecase} titlecase words.")
-            print(f"There are {uppercase} uppercase words.")
-            print(f"There are {lowercase} lowercase words.")
-            print(f"There are {numeric} numeric strings.")
-            print(f"The sum of all the numbers {sum_numbers}.")
-
-            # počet znaků v jednotlivých slovech
-            delka_slov = {}
-            for slovo in slova:
-                delka = len(slovo)
-                if delka in delka_slov:
-                    delka_slov[delka] += 1
-                else:
-                    delka_slov[delka] = 1
-
-            print(oddelovac)
-            print("LEN|\tOCCURENCES\t|NR.")
-            print(oddelovac)
-
-            # sloupcový graf
-            for i, pocet in enumerate(delka_slov):
-                if pocet > 0:
-                    stars = "*" * pocet
-                    print(f"{i+1:<3}| {stars:<13}\t|{pocet}")   
-
-    # Pokud jmeno a heslo nesedí:
-    else:
-        print(f"username: {username}\npassword: {password}\nuregistered user, terminating the program..")       
-
-# Pokud jmeno a heslo nejsou v registrovaných uživatelích:                  
+# Pokud jmeno a heslo nesedí:
 else:
     print(f"username: {username}\npassword: {password}\nuregistered user, terminating the program..")       
-                  
-
 
 
     
